88e384d6de1ec0450e2284df107f849523d26575
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2025-02-25 08:07:21 +0100

    Add: simple way to jump.

---
+++ PlatformerGame/Assets/Scripts/CameraBehavior.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class CameraBehavior : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject target;
+
+    public void LateUpdate()
+    {
+        LookAtPlayer(target);
+    }
+
+    public void LookAtPlayer(GameObject target)
+    {
+        if(target != null)
+        {
+            cam.transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10);
+        }
+    }
+}
--- PlatformerGame/Assets/Scripts/PlayerMovement.cs
+++ PlatformerGame/Assets/Scripts/PlayerMovement.cs
@@ -4,26 +4,37 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     InputAction moveAction;
+    InputAction jump;
     public float speed = 5;
     private SpriteRenderer sprite;
     private Animator animator;
+    private bool isGrounded;
+    private Rigidbody2D rb;
+    public float jumpForce = 5f;
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
+        jump = InputSystem.actions.FindAction("Jump");
         sprite = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         Vector2 moveValue = moveAction.ReadValue<Vector2>();
-        Vector3 movement = new Vector3(moveValue.x, 0, moveValue.y) * speed * Time.deltaTime;
+        Vector3 movement = new Vector3(moveValue.x, 0, 0) * speed * Time.deltaTime;
         bool isMoving = moveValue.x != 0 || moveValue.y != 0;
         animator.SetBool("isRunning", isMoving);
         transform.position += movement;
 
         FlipSprit(moveValue.x);
+
+        if(jump.IsPressed() && isGrounded)
+        {
+            Jump();
+        }
     }
 
     public void FlipSprit(float moveX)
@@ -32,5 +43,23 @@ public class PlayerMovement : MonoBehaviour
         {
             sprite.flipX = moveX < 0;
         }
+    }
+
+    public void Jump()
+    {
+        if(rb != null)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            isGrounded = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }    
     }
 }
