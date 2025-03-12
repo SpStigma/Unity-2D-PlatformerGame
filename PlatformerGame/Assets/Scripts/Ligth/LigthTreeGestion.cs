8b50b25b9f281e2a073d048afbfb36d07cf264fd
Rivet Yoann <134633530+SpStigma@users.noreply.github.com>
2025-03-12 19:55:42 +0100

    Add lighht blink and global voume not finished.

--- PlatformerGame/Assets/Scripts/Ligth/LigthTreeGestion.cs
+++ PlatformerGame/Assets/Scripts/Ligth/LigthTreeGestion.cs
@@ -2,40 +2,57 @@ using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using System.Collections.Generic;
 
-public class LigthTreeGestion : MonoBehaviour
+public class LightTreeManager : MonoBehaviour
 {
     private Camera mainCamera;
     private Light2D[] allLights;
+    private Dictionary<Light2D, float> lightOffsets = new Dictionary<Light2D, float>();
+
+    private float blinkSpeed = 1f;
 
     void Start()
     {
         mainCamera = Camera.main;
         allLights = GetComponentsInChildren<Light2D>();
-        
+
+        foreach (Light2D light in allLights)
+        {
+            lightOffsets[light] = Random.Range(0f, 1f);
+        }
     }
 
-    public void LateUpdate()
+    void LateUpdate()
     {
-        foreach(Light2D light in allLights)
+        foreach (Light2D light in allLights)
         {
-            if(light != null)
+            if (light != null)
             {
                 bool isVisible = IsLightVisible(light);
                 light.enabled = isVisible;
+
+                if (isVisible)
+                {
+                    LightBlink(light);
+                }
             }
         }
     }
 
-    public bool IsLightVisible(Light2D light)
+    private bool IsLightVisible(Light2D light)
     {
-        if(mainCamera != null)
+        if (mainCamera != null)
         {
             Vector3 viewportPoint = mainCamera.WorldToViewportPoint(light.transform.position);
-            if (viewportPoint.x > 0 && viewportPoint.x < 1 && viewportPoint.y > 0 && viewportPoint.y < 1 && viewportPoint.z > 0)
-            {
-                return true;
-            }
+            return viewportPoint.x > 0 && viewportPoint.x < 1 &&
+                   viewportPoint.y > 0 && viewportPoint.y < 1 &&
+                   viewportPoint.z > 0;
         }
         return false;
+    }
+
+    private void LightBlink(Light2D light)
+    {
+        float offset = lightOffsets[light];
+        light.falloffIntensity = Mathf.Abs(Mathf.Sin(Time.time * blinkSpeed + offset));
     }
 }
